d939dea558c638716e5dbf7ac6b976572b884735
manas-1404 <112142989+manas-1404@users.noreply.github.com>
2023-04-18 10:42:07 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,24 +3,29 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage stage)
+	{
+		StackPane root = new StackPane();
+		SchedulePane coursePane = new SchedulePane();
+		root.getChildren().add(coursePane);
+
+		Scene scene = new Scene(root, 800, 600);
+		stage.setTitle("ASU Academic Advising System");
+		stage.setScene(scene);
+		stage.show();
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	public static void main(String[] args)
+	{
+	launch(args);
+	} 
+	
+
 }
